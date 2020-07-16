0e27ba99bc83b0c1fbf45dd1dacaf86eab31a155
Md Mosleh Uddin <49858741+M0hammadASAD@users.noreply.github.com>
2020-07-16 15:59:30 +0600

    Create Codeforces Beta Round #4 (Div. 2 Only)  A. Watermelon.cpp

---
+++ Codeforces Beta Round #4 (Div. 2 Only)  A. Watermelon.cpp
@@ -0,0 +1,15 @@
+///problem : https://codeforces.com/contest/4/problem/A
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        int t;
+        cin>>t;
+        if(t==2)
+        cout<<"NO"<<endl;
+        else if(t%2==0)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+        return 0;
+    }
