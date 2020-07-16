6faffc10a3cde1bd3363b281b40d1150c356dae0
Md Mosleh Uddin <49858741+M0hammadASAD@users.noreply.github.com>
2020-07-16 16:03:57 +0600

    Create Codeforces Round #651 (Div. 2) A. Maximum GCD.cpp

---
+++ Codeforces Round #651 (Div. 2) A. Maximum GCD.cpp
@@ -0,0 +1,15 @@
+///problem : https://codeforces.com/contest/1370/problem/A
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        int t;
+        cin>>t;
+        while(t--)
+        {
+            int n,max;
+            cin>>n;
+            max=n/2;
+            cout<<max<<endl;
+        }
+    }
