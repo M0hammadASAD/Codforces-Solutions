a0587f3fcfdf441e7492877885cd438be6bc5c5c
Md Mosleh Uddin <49858741+M0hammadASAD@users.noreply.github.com>
2020-07-20 20:55:08 +0600

    Create Codeforces Round #609 (Div. 2) A. Equation.cpp

---
+++ Codeforces Round #609 (Div. 2) A. Equation.cpp
@@ -0,0 +1,14 @@
+    //problem : https://codeforces.com/contest/1269/problem/A 
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+        long long int n;
+        cin>>n;
+        if(n==1)
+            cout<<9<<" "<<8<<endl;
+        else
+            cout<<n*4<<" "<<n*3<<endl;
+     
+        return 0;
+    }
